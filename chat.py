31d675240b3775a592025adce49f73cbb9f18de8
Unknown <jkl5246@hanmail.net>
2018-05-31 21:11:06 +0900

    오류수정
    챗봇 실행오류수정

--- chat.py
+++ chat.py
@@ -13,99 +13,42 @@ app = Flask(__name__)
 def Keyboard():
     dataSend = {
         "type" : "buttons",
-        "buttons" : ["꼬리봇 도움말"],
-        "buttons" : ["꼬리봇과 대화하기"],
-        "buttons" : ["꼬리봇과 끝말잇기 게임하기"]
-    }
+        "buttons" : ["도움말","끝말잇기 게임"]
+        }
     return jsonify(dataSend)
 
 @app.route('/message', methods=['POST'])
 def Message():
     dataReceive = request.get_json()
     content = dataReceive['content']
-    if content == u"꼬리봇 도움말":
+    if content == u"도움말":
         dataSend = {
             "message": {
-                "text": "꼬리봇과 대화하기는 꼬리봇이 사용자가 한 말을 따라하는 기능입니다."
-                "text": "그리고 꼬리봇과 끝말잇기 게임하기는 꼬리봇과 끝말잇기 게임을 하는 기능입니다."
-        }
-
-        @app.route('/keyboard')
-        def Keyboard():
-            dataSend = {
+                "text": "꼬리봇은 기본적으로 사용자의 말을 따라합니다\n끝말잇기 게임은 꼬리봇과 끝말잇기 게임을 하는 기능입니다."
+        },
+        "/keyboard":{
                 "type" : "buttons",
-                "buttons" : ["꼬리봇 도움말"],
-                "buttons" : ["꼬리봇과 대화하기"],
-                "buttons" : ["꼬리봇과 끝말잇기 게임하기"]
+                "buttons" : ["도움말","끝말잇기 게임"]
             }
-
-    return jsonify(dataSend)
+        }
+    elif u"안녕" in content:
+        dataSend = {
+            "message": {
+                "text": "안녕~ 인사는 따라하지 않을거야! :("
+            }
     }
-    return jsonify(dataSend)
-
-    @app.route('/message', methods=['POST'])
-    def Message():
-        dataReceive = request.get_json()
-        content = dataReceive['content']
-        if content == u"꼬리봇과 대화하기":
-            dataSend = {
-                "message": {
-                    "text": "따라해줬으면 좋겠는 말을 입력해주세요! :)"
-                    "text": "꼬리봇은 인사는 잘 받아주고 그만이라는 말을 싫어합니다!! 처음 선택지로 돌아가려면 뒤로가기 라고 말해주세요."
+    elif u"그만" in content:
+        dataSend = {
+            "message": {
+                "text": "어떻게 그만이라고 할 수 있어..? ㅜㅜ"
             }
-        elif u"안녕" in content:
-            dataSend = {
-                "message": {
-                    "text": "안녕~ 인사는 따라하지 않을거야! :("
-                }
+    }
+    elif content == u"끝말잇기 게임":
+        dataSend = {
+            "message":{
+                "text": "끝말잇기 시작! 먼저 시작해! 그만하고 싶으면 뒤로가기 라고 말해줘\n아직 정식 서비스 준비중이라 게임이 원활하진 않을거에요ㅜㅜ"
             }
-        elif u"그만" in content:
-            dataSend = {
-                "message": {
-                    "text": "어떻게 그만이라고 할 수 있어..? ㅜㅜ"
-                }
-            }
-        elif u"뒤로가기" in content:
-            @app.route('/keyboard')
-            def Keyboard():
-                dataSend = {
-                    "type" : "buttons",
-                    "buttons" : ["꼬리봇 도움말"],
-                    "buttons" : ["꼬리봇과 대화하기"],
-                    "buttons" : ["꼬리봇과 끝말잇기 게임하기"]
-                }
-                return jsonify(dataSend)
-            }
-        else:
-            dataSend = {
-                "message": {
-                    "text": content
-                }
-            }
-        return jsonify(dataSend)
-
-
-        @app.route('/message', methods=['POST'])
-        def Message():
-            dataReceive = request.get_json()
-            content = dataReceive['content']
-            if content == u"꼬리봇과 끝말잇기 게임하기":
-                dataSend = {
-                    "message": {
-                        "text": "끝말잇기 시작! 먼저 시작해! 그만하고 싶으면 뒤로가기 라고 말해줘"
-                        "text": "아직 정식 서비스 준비중이라 게임이 원활하진 않을거에요ㅜㅜ"
-                }
-            elif u"뒤로가기" in content:
-                @app.route('/keyboard')
-                def Keyboard():
-                    dataSend = {
-                        "type" : "buttons",
-                        "buttons" : ["꼬리봇 도움말"],
-                        "buttons" : ["꼬리봇과 대화하기"],
-                        "buttons" : ["꼬리봇과 끝말잇기 게임하기"]
-                    }
-                    return jsonify(dataSend)
-
+        }
             elif u"무" == content[len(content-1)]:
                 dataSend = {
                     "message": {
@@ -262,9 +205,7 @@ def Message():
                         "text": "이건 아직 뭐라 대답해야할지 모르겠어 :( 네가 이겼어~"
                     }
                 }
-            return jsonify(dataSend)
-
-
+    return jsonify(dataSend)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = 6000)
